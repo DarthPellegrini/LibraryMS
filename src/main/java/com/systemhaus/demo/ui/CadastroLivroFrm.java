ca8532bbb1077eb201a70ca7146fb96d7474103b
DarthPellegrini <pellegrinidev@outlook.com>
2020-01-03 14:39:01 -0300

    Tamanho da tela de cadastro de livros ajustado

--- src/main/java/com/systemhaus/demo/ui/CadastroLivroFrm.java
+++ src/main/java/com/systemhaus/demo/ui/CadastroLivroFrm.java
@@ -198,7 +198,7 @@ public class CadastroLivroFrm extends SkeletonFrm{
 	protected void initComponents() {
 		iFrameCadLivro = new JInternalFrame("Cadastro de Livros", true, true);
 		iFrameCadLivro.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
-		iFrameCadLivro.setBounds(190, 35, 540, 300);
+		iFrameCadLivro.setBounds(190, 35, 580, 300);
 		
 		model = new PresentationModel<Livro>(livroSelection);
 		tablePanel = new LivroSelectionPanel(livroSelection, 1, 10);
